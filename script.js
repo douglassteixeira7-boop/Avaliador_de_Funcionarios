43154a9f4b106bfbfc04c499483e4ff3f44cfad5
douglassteixeira7-boop <aluno@senaigama.academico.local>
2026-08-11 20:54:33 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function avaliarFuncionario(){
+    let nome = document.getElementById("nome").value
+    let anos = Number(document.getElementById("anos").value)
+    let salario = Number(document.getElementById("salario").value)
+
+    let nivel;
+    let bonus;
+    let pagamento;
+
+    if (anos < 2){
+        nivel = "Júnior";
+    }else if(anos <=5 ){
+        nivel = "Pleno";
+    }else{
+        nivel = "Sênior";
+    }
+
+
+    if (salario > 2500){
+        bonus = 300;
+    }else{
+        bonus = 0;
+    }
+
+    
+    pagamento = salario + bonus;
+    
+
+    document.getElementById("resultado").innerHTML = `
+    <h2>Resultado </h2>
+    <p>Funcionário ${nome}</p>
+    <p>Nível: ${nivel}</p>
+    <p>Salário: ${salario.toFixed(2)}</p>
+    <p>Bônus: ${bonus.toFixed(2)}</p>
+    <p>Pagamento: ${pagamento.toFixed(2)}</p>
+    `
+}
